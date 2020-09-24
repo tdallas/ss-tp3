85795963e08060d4ae0286ec252b6e3353fd027a
tdallas <tdallas@itba.edu.ar>
2020-09-24 15:06:18 -0300

    still fixing parser, update gitignore

--- post-processing/main.py
+++ post-processing/main.py
@@ -2,5 +2,9 @@ from parser import Parser
 
 parser = Parser('out/output.xyz')
 output = parser.get_output()
-for particle in output[0]:
-    print(particle)
+print('choques en n', len(list(filter(lambda p:  p.get_wall_collission() == 'n' , output[5]))))
+print('choques en h', len(list(filter(lambda p:  p.get_wall_collission() == 'h' , output[5]))))
+print('choques en v', len(list(filter(lambda p:  p.get_wall_collission() == 'v' , output[5]))))
+
+# for particle in filter(lambda p:  p.get_wall_collission() == 'n' , output[0]):
+    # print(particle)
