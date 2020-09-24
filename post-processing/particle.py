0c13af911452c1489312b13fbebc12b323f7a8a6
tdallas <tdallas@itba.edu.ar>
2020-09-24 14:46:20 -0300

    added getters for parser

--- post-processing/particle.py
+++ post-processing/particle.py
@@ -5,15 +5,22 @@ class Particle():
         self.x_velocity = x_velocity
         self.y_velocity = y_velocity
         # h is horizontal wall, v is vertical wall
-        self.wall_collission = self.get_wall_collision(wall_collission)
+        self.wall_collission = wall_collission
 
     def __str__(self):
         return 'Position: [' + str(self.x_position) + ', ' + str(self.y_position) + ']\nVelocity: [' + str(self.x_velocity) + ', ' + str(self.y_velocity) + ']\nWall collission: ' + str(self.wall_collission) +'\n' 
 
-    def get_wall_collision(self, wall_collission):
-        if wall_collission == 'n':
-            return 'N'
-        elif wall_collission == 'v':
-            return 'VERTICAL'
-        else:
-            return 'HORIZONTAL'
+    def get_wall_collission(self):
+        return self.wall_collission
+
+    def get_x_velocity(self):
+        return self.x_velocity
+
+    def get_y_velocity(self):
+        return self.y_velocity
+
+    def belongs_to_left_container(self):
+        return self.x_position < 0.12
+
+    def belongs_to_right_container(self):
+        return not self.belongs_to_left_container()
