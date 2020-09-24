1cab5579031d0d7f9588f9769a1189c15e239cd5
tdallas <tdallas@itba.edu.ar>
2020-09-24 17:30:23 -0300

    final fix

--- post-processing/main.py
+++ post-processing/main.py
@@ -15,11 +15,13 @@ count =0
 temperature=0
 for iteration in output:
     for particle in filter(lambda p:  p.is_in_equilibrium() and (p.get_wall_collission() == 'v' or p.get_wall_collission() == 'h'), iteration):
-        temperature += (particle.get_velocity()**2)/2
         if particle.get_wall_collission() == 'h':
             impulse+= abs(particle.get_y_velocity()) * 2
-            count+=1
+        else:
+            impulse+= abs(particle.get_x_velocity()) * 2
+    for particle in filter(lambda p: p.is_in_equilibrium(), iteration):
+        temperature += (particle.get_velocity()**2)/2
 
-print('total pressure in system =', (impulse) / (0.48 + 300))
-print('temperature =', temperature)
+print('total pressure in system =', (impulse) / (0.48 + 0.18 + 0.08 + 10))
+print('temperature =', temperature/10)
 
