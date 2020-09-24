5265b658901e0b031f2abe811972671fdc9ea3dd
tdallas <tdallas@itba.edu.ar>
2020-09-24 15:19:08 -0300

    added impulse calculation

--- post-processing/main.py
+++ post-processing/main.py
@@ -2,9 +2,27 @@ from parser import Parser
 
 parser = Parser('out/output.xyz')
 output = parser.get_output()
-print('choques en n', len(list(filter(lambda p:  p.get_wall_collission() == 'n' , output[5]))))
-print('choques en h', len(list(filter(lambda p:  p.get_wall_collission() == 'h' , output[5]))))
-print('choques en v', len(list(filter(lambda p:  p.get_wall_collission() == 'v' , output[5]))))
 
-# for particle in filter(lambda p:  p.get_wall_collission() == 'n' , output[0]):
-    # print(particle)
+impulse_left_dt = []
+impulse_right_dt = []
+
+
+impulse_left = 0
+impulse_right = 0
+for iteration in output:
+    for particle in filter(lambda p:  p.get_wall_collission() == 'v' or p.get_wall_collission() == 'h', iteration):
+        if particle.belongs_to_left_container():
+            if particle.get_wall_collission() == 'v':
+                impulse_left += (abs(particle.get_y_velocity()) * 2)
+            else:
+                impulse_right += (abs(particle.get_x_velocity()) * 2)
+        else:
+            if particle.get_wall_collission() == 'v':
+                impulse_left += (abs(particle.get_y_velocity()) * 2)
+            else:
+                impulse_right += (abs(particle.get_x_velocity()) * 2)
+    impulse_left_dt.append(impulse_left)
+    impulse_right_dt.append(impulse_right)
+
+print('impulse_left_dt', impulse_left_dt)
+print('impulse_right_dt', impulse_right_dt)
--- post-processing/particle.py
+++ post-processing/particle.py
@@ -1,9 +1,9 @@
 class Particle():
     def __init__(self, x_position, y_position, x_velocity, y_velocity, wall_collission):
-        self.x_position = x_position
-        self.y_position = y_position
-        self.x_velocity = x_velocity
-        self.y_velocity = y_velocity
+        self.x_position = float(x_position)
+        self.y_position = float(y_position)
+        self.x_velocity = float(x_velocity)
+        self.y_velocity = float(y_velocity)
         # h is horizontal wall, v is vertical wall
         self.wall_collission = wall_collission
 
